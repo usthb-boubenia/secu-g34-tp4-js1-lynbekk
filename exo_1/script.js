3a12dec614ffb1f0b5a8813508b0d924856510ca
lynbekk <bekkailyna@gmail.com>
2025-10-28 11:50:49 +0100

    TP4 JavaScript terminé

---
+++ exo_1/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// Déclaration des fonctions vues en cours (diapo 45 à 52)
+// Chaque fonction est liée à un bouton HTML via l'attribut "onclick"
+function f1(){
+    console.log("John");
+}
+function f2(){
+    console.log("Doe");
+}
+function f3(){
+    console.log("John-Doe");
+}
+function f4(){
+    console.log("30");
+}
+function f5(){
+    console.log("15");
+}
+function f6(){
+    console.log("3.14");
+}
